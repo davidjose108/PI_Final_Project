ad1c24a9789199f9b5c385f4c38f0d8c638dcd4a
David Garrido <115107126+davidjose108@users.noreply.github.com>
2023-06-01 19:14:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,31 @@
 
 
 # Convert file into a dataframe
-df = pd.read_csv(r'C:\Users\v-davidgarr\Documents\Data_Volume_042023.csv')
+import pandas as pd
+
+def CSV_to_DF(file):
+# Convert file into a dataframe
+    df = pd.read_csv(file)
 
 # Rewriting the df with the important rows 
-df_bookeddata_month1 = df.loc[df["Product description"] == "INFO ZU IHREM DATENVOLUMEN DES ABRECHNUNGSMONATS IM INLAND VERTRAGLICH VEREINBARTES DATENVOLUMEN"]
+    df_bookeddata = df.loc[df["Product description"] == "INFO ZU IHREM DATENVOLUMEN DES ABRECHNUNGSMONATS IM INLAND VERTRAGLICH VEREINBARTES DATENVOLUMEN"]
 
 # Keeping the columns that are important
-df_bookeddata_month1 = df_bookeddata_month1.filter(["Period", "Phone number", "Userid", "Volume in KiB"])
+    df_bookeddata = df_bookeddata.filter(["Period", "Phone number", "Userid", "Volume in KiB"])
 
 # Renaming the columns/ inplace: Makes changes in original Data Frame if True.
-df_bookeddata_month1.rename(columns={'Volume in KiB': 'Booked Data Volume for Month 1'}, inplace = True)
+    df_bookeddata.rename(columns={'Volume in KiB': 'Booked Data Volume'}, inplace = True)
 
-df_useddata_month1= df.loc[df["Product description"] == "INFO ZU IHREM DATENVOLUMEN DES ABRECHNUNGSMONATS IM INLAND VERBRAUCHTES DATENVOLUMEN MIT HOHER GESCHWINDIGKEIT"]
-df_useddata_month1 = df_useddata_month1.filter(["Phone number", "Volume in KiB"])
-df_useddata_month1.rename(columns={'Volume in KiB': 'Used Data Volume for Month 1'}, inplace = True)
+    df_useddata= df.loc[df["Product description"] == "INFO ZU IHREM DATENVOLUMEN DES ABRECHNUNGSMONATS IM INLAND VERBRAUCHTES DATENVOLUMEN MIT HOHER GESCHWINDIGKEIT"]
+    df_useddata = df_useddata.filter(["Phone number", "Volume in KiB"])
+    df_useddata.rename(columns={'Volume in KiB': 'Used Data Volume'}, inplace = True)
 
 # Merging the data frames 
 
-df_month1 = pd.merge(df_bookeddata_month1, df_useddata_month1, on='Phone number')
-print(df_month1)
+    df_month = pd.merge(df_bookeddata, df_useddata, on='Phone number')
+    return df_month
+
+df_month1 = CSV_to_DF(r'C:\Users\v-davidgarr\Documents\Data Adjustment_Python Project\Data_Volume_042023.csv')
+print (df_month1)
+df_month2 = CSV_to_DF(r'C:\Users\v-davidgarr\Documents\Data Adjustment_Python Project\Data_Volume_032023.csv')
+print(df_month2)
