90264e1c990151c8a388f5921fe0136d600611e8
David Garrido <115107126+davidjose108@users.noreply.github.com>
2023-06-02 19:14:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 #The purporse of this project is to automatize the mobile data plan adjustment in a big company according to the data consumption pf each user of the last three months. 
 
 
-# Convert file into a dataframe
 import pandas as pd
+import statistics 
 
 def CSV_to_DF(file):
 # Convert file into a dataframe
@@ -50,4 +50,35 @@ df_month3=filter_DF(df_month3)
 rename_DF(df_month3,3)
 
 df_final=pd.merge(pd.merge(df_month1,df_month2,on='Phone number'),df_month3,on='Phone number')
-print(df_final)
+
+# Iterating over the Data Frame 
+
+for i in df_final.index:
+    data_consumption = [float(df_final.loc[i,"Used Data Volume for Month 1"]), float(df_final.loc[i,"Used Data Volume for Month 2"]), float(df_final.loc[i,"Used Data Volume for Month 3"])]
+   # print (data_consumption)
+    data_consumption_average = statistics.mean(data_consumption)
+    #print (data_consumption)
+    data_consumption_stdv = statistics.stdev(data_consumption)
+    #print(data_consumption_stdv)
+    data_plan = [df_final.loc[i,"Booked Data Volume for Month 1"], df_final.loc[i,"Booked Data Volume for Month 2"], df_final.loc[i,"Booked Data Volume for Month 3"]]
+    data_plan_average = statistics.mean(data_plan)
+    #print (data_plan)
+    def new_data_plan(y):
+        data_plan_list = [8, 10, 12, 15, 20, 30, 50]
+        closest = min(data_plan_list, key=lambda x: abs(x-y))
+        return closest 
+
+    discrepancy = data_consumption_average - data_plan_average
+
+    if df_final.loc[i,"Booked Data Volume for Month 1"] != df_final.loc[i,"Used Data Volume for Month 2"] or df_final.loc[i,"Booked Data Volume for Month 1"] != df_final.loc[i,"Used Data Volume for Month 3"]:
+        a = 1 #print ('It is not possible to propose a data plan adjustment. \nThe data plan has been changed in the last three months.')
+    else:
+        if data_consumption_stdv >= 5.00:
+            a = 1 # print ('It is not possible to propose a data plan adjustment. \nThere is too much difference between the data consumption.')
+        else:
+            if -10 < (data_consumption_average - data_plan_average) < 10:
+                a = 1 #print ('It is not necessary to propose a data plan adjustment. \nThe current data plan and data consumption are too close. ')
+            else:
+                print (f'The proposed data plan for this phone number is: {new_data_plan(data_consumption)} GB')
+
+
